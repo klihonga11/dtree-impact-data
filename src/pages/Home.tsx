82497cc94a5a03025863767521741539d3a1edd3
klihonga11 <klihonga11@gmail.com>
2026-01-26 05:48:13 +0200

    Added a nav bar

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,24 @@
 import { useNavigate } from "react-router-dom";
-import { Button } from "@mantine/core";
 import { useAuth } from "../hooks/useAuth";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import classes from "../css/NavbarSimple.module.css";
+
+const data = [
+  { link: "", label: "1. Individuals Served" },
+  { link: "", label: "2. Improved coordination" },
+  { link: "", label: "3. Government supported" },
+  { link: "", label: "4. Health visits supported" },
+  { link: "", label: "5. Health workers supported" },
+  { link: "", label: "6. Access" },
+  { link: "", label: "7. Continuity" },
+  { link: "", label: "8. Efficiency" },
+  { link: "", label: "9. Individual Agency" },
+  { link: "", label: "10. Effectiveness" },
+];
 
 export default function HomePage() {
   const { isAuthenticated, logout } = useAuth();
+  const [active, setActive] = useState("1. Individuals Served");
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -13,16 +27,35 @@ export default function HomePage() {
     }
   }, [isAuthenticated, navigate]);
 
-  const handleLogout = async () => {
+  const handleLogout = async (event: React.MouseEvent) => {
+    event.preventDefault();
     await logout();
   };
 
+  const links = data.map((item) => (
+    <a
+      className={classes.link}
+      data-active={item.label === active || undefined}
+      href={item.link}
+      key={item.label}
+      onClick={(event) => {
+        event.preventDefault();
+        setActive(item.label);
+      }}
+    >
+      <span>{item.label}</span>
+    </a>
+  ));
+
   return (
-    <>
-      <div>
-        <h1>Welcome!</h1>
-        <Button onClick={handleLogout}>Log out</Button>
+    <nav className={classes.navbar}>
+      <div className={classes.navbarMain}>{links}</div>
+
+      <div className={classes.footer}>
+        <a href="#" className={classes.link} onClick={handleLogout}>
+          <span>Logout</span>
+        </a>
       </div>
-    </>
+    </nav>
   );
 }
