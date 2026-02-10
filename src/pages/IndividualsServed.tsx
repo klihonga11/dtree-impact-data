a9d8d06c126e3378467587b0e6fbcd1064cd3bde
klihonga11 <klihonga11@gmail.com>
2026-02-10 04:52:38 +0200

    Load programs from API

--- src/pages/IndividualsServed.tsx
+++ src/pages/IndividualsServed.tsx
@@ -1,5 +1,53 @@
-import { Text } from "@mantine/core";
+import { Button, NativeSelect } from "@mantine/core";
+import { useState } from "react";
 
 export default function IndividualsServed() {
-  return <Text>Individuals Served</Text>;
+  type Program = {
+    id: string;
+    displayName: string;
+  };
+
+  type ProgramsResponse = {
+    pager: {
+      page: number;
+      total: number;
+      pageSize: number;
+      pageCount: number;
+    };
+    programs: Program[];
+  };
+
+  const [programs, setPrograms] = useState<Program[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+
+  const getPrograms = async () => {
+    try {
+      const response = await fetch("/dhis2/api/programs", {
+        credentials: "include",
+      });
+
+      if (!response.ok) {
+        throw new Error("Failed to get programs");
+      }
+
+      const data: ProgramsResponse = await response.json();
+      setPrograms(data.programs);
+      setIsLoading(false);
+    } catch (error) {
+      console.log("Error", error);
+    }
+  };
+
+  return (
+    <>
+      <Button onClick={getPrograms}>Click me!</Button>
+      <NativeSelect
+        disabled={isLoading}
+        data={programs.map((p) => ({
+          value: p.id,
+          label: p.displayName,
+        }))}
+      />
+    </>
+  );
 }
