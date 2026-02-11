bd17676b075e65b778169458671c3297f3e4abdd
klihonga11 <klihonga11@gmail.com>
2026-02-11 04:39:05 +0200

    Load organisation units from API

--- src/pages/IndividualsServed.tsx
+++ src/pages/IndividualsServed.tsx
@@ -1,4 +1,4 @@
-import { Button, NativeSelect } from "@mantine/core";
+import { Button, MultiSelect, NativeSelect } from "@mantine/core";
 import { useState } from "react";
 
 export default function IndividualsServed() {
@@ -17,7 +17,25 @@ export default function IndividualsServed() {
     programs: Program[];
   };
 
+  type OrganisationUnit = {
+    id: string;
+    displayName: string;
+  };
+
+  type OrganisationUnitResponse = {
+    pager: {
+      page: number;
+      total: number;
+      pageSize: number;
+      pageCount: number;
+    };
+    organisationUnits: OrganisationUnit[];
+  };
+
   const [programs, setPrograms] = useState<Program[]>([]);
+  const [organisationUnit, setOrganisationUnits] = useState<OrganisationUnit[]>(
+    []
+  );
   const [isLoading, setIsLoading] = useState<boolean>(true);
 
   const getPrograms = async () => {
@@ -32,18 +50,55 @@ export default function IndividualsServed() {
 
       const data: ProgramsResponse = await response.json();
       setPrograms(data.programs);
-      setIsLoading(false);
     } catch (error) {
       console.log("Error", error);
     }
   };
 
+  const getOrganizationUnits = async () => {
+    const response = await fetch(
+      "/dhis2/api/organisationUnits?filter=level:in:[1,3]",
+      {
+        credentials: "include",
+      }
+    );
+
+    if (!response.ok) {
+      throw new Error("Failed to get organisation units");
+    }
+
+    const data: OrganisationUnitResponse = await response.json();
+    setOrganisationUnits(data.organisationUnits);
+  };
+
+  const downloadData = async () => {
+    try {
+      setIsLoading(true);
+      await getPrograms();
+      await getOrganizationUnits();
+    } catch (error) {
+      console.log("Error", error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
   return (
     <>
-      <Button onClick={getPrograms}>Click me!</Button>
+      <Button onClick={downloadData}>Click me!</Button>
       <NativeSelect
+        label="Programs"
         disabled={isLoading}
         data={programs.map((p) => ({
+          value: p.id,
+          label: p.displayName,
+        }))}
+      />
+
+      <MultiSelect
+        label="Organisation units"
+        disabled={isLoading}
+        data={organisationUnit.map((p) => ({
           value: p.id,
           label: p.displayName,
         }))}
