98fedbe77637b21d8b3e02114ebdd42d13e2b93b
klihonga11 <klihonga11@gmail.com>
2026-03-13 04:04:04 +0200

    Moved logic into its own files

--- src/pages/IndividualsServed.tsx
+++ src/pages/IndividualsServed.tsx
@@ -1,122 +1,9 @@
-import {
-  Button,
-  Center,
-  Group,
-  Loader,
-  MultiSelect,
-  NativeSelect,
-  Space,
-  Table,
-} from "@mantine/core";
-import { DatePickerInput } from "@mantine/dates";
-import { useState } from "react";
-import type {
-  IndividualsServedResponse,
-  OrganisationUnit,
-  Program,
-} from "../utils/types";
-import CustomAlert from "../components/Alert";
+import { Center, Loader, Space, Table } from "@mantine/core";
+import DataFilter from "../components/DataFilter";
+import { useDataFilter } from "../hooks/useDataFilter";
 
 export default function IndividualsServed() {
-  const [dateRange, setDateRange] = useState<[string | null, string | null]>([
-    null,
-    null,
-  ]);
-
-  const programs: Program[] = JSON.parse(
-    localStorage.getItem("programs") ?? "[]",
-  );
-  const [selectedProgramId, setSelectedProgramId] = useState<string>("");
-  const [selectedProgramStageId, setSelectedProgramStageId] =
-    useState<string>("");
-
-  const organisationUnits: OrganisationUnit[] = JSON.parse(
-    localStorage.getItem("organisationUnits") ?? "[]",
-  );
-  const [selectedOrganisationUnits, setSelectedOrganisationUnits] = useState<
-    string[]
-  >([]);
-
-  type RowType = {
-    id: string;
-    district: string;
-    count: number;
-  };
-  const [tableRows, setTableRows] = useState<RowType[]>([]);
-
-  const [isLoading, setIsLoading] = useState<boolean>(false);
-
-  const [isAlertVisible, setIsAlertVisible] = useState<boolean>(false);
-  const [alertMessage, setAlertMessage] = useState<string>("");
-
-  const applyFilters = () => {
-    if (selectedProgramId == "") {
-      setAlertMessage("Please select a program");
-      setIsAlertVisible(true);
-      return;
-    }
-
-    if (selectedOrganisationUnits.length == 0) {
-      setAlertMessage("Please select an organisation unit");
-      setIsAlertVisible(true);
-      return;
-    }
-
-    if (dateRange[0] === null || dateRange[1] === null) {
-      setAlertMessage("Please select a date range");
-      setIsAlertVisible(true);
-      return;
-    }
-
-    populateTable();
-  };
-
-  const populateTable = async () => {
-    try {
-      setTableRows([]); // clear the table
-      setIsLoading(true);
-
-      const tempRows = [];
-      for (const ou of selectedOrganisationUnits) {
-        const data = await getData(ou);
-        tempRows.push(data);
-      }
-
-      setTableRows(tempRows); // update the table
-    } catch (error) {
-      console.log("Error", error);
-    } finally {
-      setIsLoading(false);
-    }
-  };
-
-  const getData = async (organisationUnitId: string): Promise<RowType> => {
-    try {
-      let url = "";
-      if (selectedProgramStageId == "") {
-        url = `/dhis2/api/analytics/events/query/${selectedProgramId}?startDate=${dateRange[0]}&endDate=${dateRange[1]}&dimension=ou:${organisationUnitId}&outputType=ENROLLMENT&pageSize=1&totalPages=true`;
-      } else {
-        url = `/dhis2/api/analytics/events/query/${selectedProgramId}?stage=${selectedProgramStageId}&startDate=${dateRange[0]}&endDate=${dateRange[1]}&dimension=ou:${organisationUnitId}&outputType=ENROLLMENT&pageSize=1&totalPages=true`;
-      }
-
-      const response = await fetch(url, {
-        credentials: "include",
-      });
-
-      const data: IndividualsServedResponse = await response.json();
-
-      return {
-        id: organisationUnitId,
-        district:
-          organisationUnits.find((ou) => ou.id === organisationUnitId)
-            ?.displayName ?? "",
-        count: data.metaData.pager.pageCount,
-      };
-    } catch (error) {
-      console.log("Error", error);
-      throw error;
-    }
-  };
+  const { isLoading, tableRows, fetchData } = useDataFilter();
 
   const rows = tableRows.map((element) => (
     <Table.Tr key={element.id}>
@@ -125,86 +12,9 @@ export default function IndividualsServed() {
     </Table.Tr>
   ));
 
-  const onSelectOrgUnit = (selected: string[]) => {
-    if (selected.includes("all")) {
-      setSelectedOrganisationUnits(organisationUnits.map((ou) => ou.id));
-      return;
-    }
-
-    setSelectedOrganisationUnits(selected);
-  };
-
   return (
     <>
-      <Group justify="space-between" align="flex-end" grow>
-        <NativeSelect
-          label="*Program"
-          styles={{ input: { color: "black" } }}
-          data={[
-            { value: "", label: "", disabled: true },
-            ...programs.map((p) => ({
-              value: p.id,
-              label: p.name,
-            })),
-          ]}
-          value={selectedProgramId}
-          onChange={(event) => setSelectedProgramId(event.target.value)}
-        />
-
-        <NativeSelect
-          label="Program stage"
-          data={[
-            { value: "", label: "" },
-            ...(programs
-              .find((p) => p.id === selectedProgramId)
-              ?.programStages.map((ps) => ({
-                value: ps.id,
-                label: ps.name,
-              })) ?? []),
-          ]}
-          value={selectedProgramStageId}
-          onChange={(event) => setSelectedProgramStageId(event.target.value)}
-        />
-
-        <MultiSelect
-          label="*Organisation unit"
-          data={[
-            { value: "all", label: "All districts" },
-            ...organisationUnits.map((ou) => ({
-              value: ou.id,
-              label: ou.displayName,
-            })),
-          ]}
-          value={selectedOrganisationUnits}
-          onChange={onSelectOrgUnit}
-          clearable
-          leftSection={
-            selectedOrganisationUnits.length > 0 ? (
-              <span
-                style={{ fontSize: 14, color: "black", whiteSpace: "nowrap" }}
-              >
-                {selectedOrganisationUnits.length} districts selected
-              </span>
-            ) : null
-          }
-          leftSectionWidth={selectedOrganisationUnits.length > 0 ? 160 : 0}
-          styles={{
-            pill: { display: "none" },
-            inputField: { display: "none" },
-          }}
-        />
-
-        <DatePickerInput
-          type="range"
-          label="*Date range"
-          value={dateRange}
-          onChange={setDateRange}
-        />
-
-        <Button onClick={applyFilters} disabled={isLoading}>
-          Apply
-        </Button>
-      </Group>
+      <DataFilter getData={fetchData} />
 
       <Space h="lg" />
 
@@ -230,13 +40,6 @@ export default function IndividualsServed() {
             </Table.Tr>
           </Table.Tbody>
         </Table>
-      )}
-
-      {isAlertVisible && (
-        <CustomAlert
-          message={alertMessage}
-          onClose={() => setIsAlertVisible(false)}
-        />
       )}
     </>
   );
