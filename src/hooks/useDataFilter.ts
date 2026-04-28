7083f95e8c36ba4962f6b7a57189091cd7888672
klihonga11 <klihonga11@gmail.com>
2026-04-28 08:33:25 +0200

    Update to get user data

--- src/hooks/useDataFilter.ts
+++ src/hooks/useDataFilter.ts
@@ -1,29 +1,35 @@
 import { useState } from "react";
-import type { AnalyticsEventResponse, TableRowType } from "../utils/types";
+import type {
+  AnalyticsEventResponse,
+  TableRowType,
+  DataFilterData,
+  UserResponse,
+} from "../utils/types";
 
 type DataFilterReturnType = {
   isLoading: boolean;
   tableRows: TableRowType[];
-  fetchData: (urls: string[]) => Promise<void>;
+  fetchData: (data: DataFilterData[]) => Promise<void>;
+  fetchUserData: (data: DataFilterData[]) => Promise<void>;
 };
 
 export const useDataFilter = (): DataFilterReturnType => {
   const [isLoading, setIsLoading] = useState<boolean>(false);
   const [tableRows, setTableRows] = useState<TableRowType[]>([]);
 
-  const fetchData = async (urls: string[]) => {
+  const fetchData = async (data: DataFilterData[]) => {
     try {
       const promises: Promise<AnalyticsEventResponse>[] = [];
-      urls.forEach((url) => {
+      data.forEach((d) => {
         promises.push(
-          fetch(url, { credentials: "include" }).then((result) =>
+          fetch(d.url, { credentials: "include" }).then((result) =>
             result.json(),
           ),
         );
       });
 
       setIsLoading(true);
-      await loadResults(promises);
+      await loadResults(promises, data);
     } catch (error) {
       console.log("Error", error);
     } finally {
@@ -31,21 +37,61 @@ export const useDataFilter = (): DataFilterReturnType => {
     }
   };
 
-  const loadResults = async (promises: Promise<AnalyticsEventResponse>[]) => {
+  const loadResults = async (
+    promises: Promise<AnalyticsEventResponse>[],
+    data: DataFilterData[],
+  ) => {
     const results = await Promise.all(promises);
 
     const rows: TableRowType[] = [];
-    results.forEach((data) => {
-      const ou = data.metaData.dimensions.ou[0];
+    results.forEach((result, index) => {
       rows.push({
-        id: ou,
-        district: data.metaData.items[ou].name,
-        count: data.metaData.pager.pageCount,
+        id: data[index].orgUnitId,
+        district: data[index].orgUnitName,
+        count: result.metaData.pager.pageCount,
       });
     });
 
     setTableRows(rows);
   };
 
-  return { isLoading, tableRows, fetchData };
+  const fetchUserData = async (userData: DataFilterData[]) => {
+    try {
+      const promises: Promise<UserResponse>[] = [];
+      userData.forEach((ud) => {
+        promises.push(
+          fetch(ud.url, { credentials: "include" }).then((result) =>
+            result.json(),
+          ),
+        );
+      });
+
+      setIsLoading(true);
+      await loadUserResults(promises, userData);
+    } catch (error) {
+      console.log("Error", error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  const loadUserResults = async (
+    promises: Promise<UserResponse>[],
+    userData: DataFilterData[],
+  ) => {
+    const results = await Promise.all(promises);
+
+    const rows: TableRowType[] = [];
+    results.forEach((data, index) => {
+      rows.push({
+        id: userData[index].orgUnitId,
+        district: userData[index].orgUnitName,
+        count: data.pager.total,
+      });
+    });
+
+    setTableRows(rows);
+  };
+
+  return { isLoading, tableRows, fetchData, fetchUserData };
 };
