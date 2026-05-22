adaf3dc58e3ce17044c9957841f1861776e3695e
Khumbo Lihonga <klihonga@d-tree.org>
2026-05-22 08:24:50 +0200

    Update name of variable

--- src/pages/Continuty.tsx
+++ src/pages/Continuty.tsx
@@ -2,7 +2,7 @@ import { Center, Loader, Space } from "@mantine/core";
 import DataFilter from "../components/DataFilter";
 import DistrictCountTable from "../components/DistrictCountTable";
 import { useDataFilter } from "../hooks/useDataFilter";
-import { CBMNC_WOMAN_PROGRAM_ID, CONTINUITY_DATA_ELEMENT_ID, ENDPOINT_ANALYTICS_EVENTS, OUTPUT_TYPE_EVENT, PREGNANCY_PERIOD_PROGRAM_STAGE_ID } from "../utils/static";
+import { CBMNC_WOMAN_PROGRAM_ID, WOMAN_VISITED_THE_FACILITY_DATA_ELEMENT_ID, ENDPOINT_ANALYTICS_EVENTS, OUTPUT_TYPE_EVENT, PREGNANCY_PERIOD_PROGRAM_STAGE_ID } from "../utils/static";
 
 export default function Continuity() {
     const { isLoading, tableRows, fetchData } = useDataFilter();
@@ -17,7 +17,7 @@ export default function Continuity() {
                 programStageDisabled
                 defaultProgramId={CBMNC_WOMAN_PROGRAM_ID}
                 defaultProgramStageId={PREGNANCY_PERIOD_PROGRAM_STAGE_ID}
-                defaultDataElementId={CONTINUITY_DATA_ELEMENT_ID}
+                defaultDataElementId={WOMAN_VISITED_THE_FACILITY_DATA_ELEMENT_ID}
                 defaultDataElementValue="1"
             />
     
