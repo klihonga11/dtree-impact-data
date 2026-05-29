890fefe0cb22c691f99fc5b92359e504cae945f7
Khumbo Lihonga <klihonga@d-tree.org>
2026-05-29 08:40:54 +0200

    Remove switch from table head

--- src/components/DistrictCountTable.tsx
+++ src/components/DistrictCountTable.tsx
@@ -26,32 +26,29 @@ export default function DistrictCountTable({
   //TODO: Reduce the width of the number column
 
   return (
-    <Table.ScrollContainer minWidth={500} maxHeight={525}>
-      <Table highlightOnHover stickyHeader>
-        <Table.Thead>
-          <Table.Tr>
-            <Table.Td>
-              <Switch p={8} checked={hideZeroes} onChange={(event) => setHideZeroes(event.currentTarget.checked)} label="Hide zeroes"/>
-            </Table.Td>
-            
-          </Table.Tr>
-          <Table.Tr>
-            <Table.Th style={{ width: 1, whiteSpace: 'nowrap' }}>#</Table.Th>
-            <Table.Th>{locationType === LOCATION_TYPE_ORG_UNIT ? "District" : "User group"}</Table.Th>
-            <Table.Th >Count</Table.Th>
-          </Table.Tr>
-        </Table.Thead>
-        <Table.Tbody>
-          {rows}
-          <Table.Tr style={{ borderTop: "2px solid #000" }}>
-            <Table.Td fw={700}></Table.Td>
-            <Table.Td fw={700}>TOTAL</Table.Td>
-            <Table.Td fw={700}>
-              {tableRows.reduce((sum, item) => sum + item.count, 0)}
-            </Table.Td>
-          </Table.Tr>
-        </Table.Tbody>
-      </Table>
-    </Table.ScrollContainer>
+    <>
+      <Switch p={8} checked={hideZeroes} onChange={(event) => setHideZeroes(event.currentTarget.checked)} label="Hide zeroes"/>
+      <Table.ScrollContainer minWidth={500} maxHeight={525}>
+        <Table highlightOnHover stickyHeader>
+          <Table.Thead>
+            <Table.Tr>
+              <Table.Th style={{ width: 1, whiteSpace: 'nowrap' }}>#</Table.Th>
+              <Table.Th>{locationType === LOCATION_TYPE_ORG_UNIT ? "District" : "User group"}</Table.Th>
+              <Table.Th >Count</Table.Th>
+            </Table.Tr>
+          </Table.Thead>
+          <Table.Tbody>
+            {rows}
+            <Table.Tr style={{ borderTop: "2px solid #000" }}>
+              <Table.Td fw={700}></Table.Td>
+              <Table.Td fw={700}>TOTAL</Table.Td>
+              <Table.Td fw={700}>
+                {tableRows.reduce((sum, item) => sum + item.count, 0)}
+              </Table.Td>
+            </Table.Tr>
+          </Table.Tbody>
+        </Table>
+      </Table.ScrollContainer>
+    </>
   );
 }
